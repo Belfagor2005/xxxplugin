894268ebc8f32073ef95d23d9ff5b8beaa95b479
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-08-13 13:06:40 +0200

    add milfzr

--- usr/lib/enigma2/python/Plugins/Extensions/xxxplugin/Sites/milfzr.py
+++ usr/lib/enigma2/python/Plugins/Extensions/xxxplugin/Sites/milfzr.py
@@ -28,6 +28,7 @@ from Plugins.Extensions.xxxplugin.plugin import rvList, Playstream1
 from Plugins.Extensions.xxxplugin.plugin import rvoneListEntry
 from Plugins.Extensions.xxxplugin.plugin import show_
 from Plugins.Extensions.xxxplugin.lib import Utils
+from Plugins.Extensions.xxxplugin.lib import html_conv
 from Plugins.Extensions.xxxplugin import _, skin_path
 PY3 = sys.version_info.major >= 3
 
@@ -38,16 +39,17 @@ if sys.version_info >= (2, 7, 9):
         sslContext = None
 
 currversion = '1.0'
-title_plug = 'Pornbimbo '
-desc_plugin = ('..:: Pornbimbo by Lululla %s ::.. ' % currversion)
+title_plug = 'milfzr '
+desc_plugin = ('..:: milfzr by Lululla %s ::.. ' % currversion)
 PLUGIN_PATH = resolveFilename(SCOPE_PLUGINS, "Extensions/{}".format('xxxplugin'))
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 print(current)
 print(parent)
-pluglogo = os.path.join(PLUGIN_PATH, 'pic/pornobimbo.png')
-stripurl = 'aHR0cDovL3Bvcm5iaW1iby5jb20vY2F0ZWdvcmllcy8='
+pluglogo = os.path.join(PLUGIN_PATH, 'pic/milfzr.png')
+stripurl = 'aHR0cHM6Ly93d3cubWlsZnpyLmNvbS8'
+referer = 'https://www.milfzr.com/'
 _session = None
 Path_Movies = '/tmp/'
 
@@ -56,7 +58,7 @@ search = False
 
 
 Panel_list = [
-    ('TOP RATED'),
+    ('TAGS'),
     ('CATEGORY'),
     ('SEARCH'),
     ]
@@ -119,10 +121,12 @@ class main(Screen):
         if result:
             global search
             name = str(result)
-            url = self.urlx + str(result) + '/'
+            # url = self.urlx + "/" + str(page) + "/?sort=re"
+            # https://milfzr.com/page/2/?s=anal
+            url = self.urlx + '/page/npage/?s=' + str(result)
             try:
                 search = True
-                self.session.open(pornbimbo2, name, url)
+                self.session.open(milfzrx, name, url)
             except:
                 return
         else:
@@ -137,20 +141,25 @@ class main(Screen):
         self.keyNumberGlobalCB(self['menulist'].getSelectedIndex())
 
     def keyNumberGlobalCB(self, idx):
-        global namex, lnk
+        global namex
         namex = ''
         sel = self.menu_list[idx]
-        if sel == ("CATEGORY"):
-            namex = "CATEGORY"
-            self.session.open(pornbimbo, namex, lnk)
+
+        if sel == ("TAGS"):
+            namex = "Tags"
+            lnk = 'https://milfzr.com/'
+            self.session.open(milfzr, namex, lnk)
+        elif sel == ("CATEGORY"):
+            namex = "Category"
+            lnk = 'https://milfzr.com/'
+            self.session.open(milfzr, namex, lnk)
+
         elif sel == ("SEARCH"):
             namex = "Search"
-            lnk = 'http://pornbimbo.com/search/'
+            lnk = 'https://milfzr.com/'
             self.search_text(namex, lnk)
-        if sel == ("TOP RATED"):
-            namex = "Top"
-            lnk = 'http://pornbimbo.com/models/'
-            self.session.open(pornbimbo, namex, lnk)
+        else:
+            return
 
     def up(self):
         self[self.currentList].up()
@@ -181,7 +190,7 @@ class main(Screen):
             self.close()
 
 
-class pornbimbo(Screen):
+class milfzr(Screen):
     def __init__(self, session, name, url):
         self.session = session
         Screen.__init__(self, session)
@@ -212,7 +221,7 @@ class pornbimbo(Screen):
                                                                 'left': self.left,
                                                                 'right': self.right,
                                                                 'ok': self.ok,
-                                                                # 'green': self.message2,
+                                                                'green': self.ok,
                                                                 'cancel': self.exit,
                                                                 'red': self.exit}, -1)
         self.timer = eTimer()
@@ -245,18 +254,28 @@ class pornbimbo(Screen):
     def updateMenuList(self):
         self.cat_list = []
         try:
-            url = Utils.b64decoder(stripurl)
-            content = Utils.getUrl(url)
+            # content = Utils.ReadUrl2(self.url, referer)
+            content = Utils.getUrl(self.url)
             if six.PY3:
                 content = six.ensure_str(content)
-            regexcat = 'a class="item" href="(.*?)" title="(.*?)".*?img class="thumb" src="(.*?)"'
-            match = re.compile(regexcat, re.DOTALL).findall(content)
-            for url, name, pic in match:
-                url1 = url
-                name = name.replace('">', '')
-                name = name.upper()
-                pic = pic
-                self.cat_list.append(show_(name, url1))
+            if "tag" in self.name.lower():
+                regexcat = '<a href="https://milfzr.com/tag/(.*?)".*?aria-label="(.*?)"'
+                match = re.compile(regexcat, re.DOTALL).findall(content)
+                for url, name in match:
+                    url1 = 'https://milfzr.com/tag/' + str(url)
+                    name = html_conv.html_unescape(name)
+                    print('tag 0 url= ', url1)
+                    self.cat_list.append(show_(name, url1))
+            elif 'category' in self.name.lower():
+                # https://milfzr.com/category/all-in-family/
+                # <a href="https://milfzr.com/category/asian-milf/" title="
+                regexcat = 'https://milfzr.com/category/(.*?)".*?title="(.*?)"'
+                match = re.compile(regexcat, re.DOTALL).findall(content)
+                for url, name in match:
+                    url1 = "https://milfzr.com/category/" + url  # name + "/"
+                    name = html_conv.html_unescape(name)
+                    print('category 0 url= ', url1)
+                    self.cat_list.append(show_(name, url1))
             if len(self.cat_list) < 0:
                 return
             else:
@@ -273,13 +292,13 @@ class pornbimbo(Screen):
         self.play_that_shit(url, name)
 
     def play_that_shit(self, url, name):
-        self.session.open(pornbimbox, name, url)
+        self.session.open(milfzrx, name, url)
 
     def exit(self):
         self.close()
 
 
-class pornbimbox(Screen):
+class milfzrx(Screen):
     def __init__(self, session, name, url):
         self.session = session
         Screen.__init__(self, session)
@@ -313,13 +332,13 @@ class pornbimbox(Screen):
     def _gotPageLoad(self):
         self.cat_list = []
         name = self.name
-        url = self.url
         try:
-            pages = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+            pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+            # https://milfzr.com/page/2/?s=
+            # self.urlx + '/page/npage/?s=' + str(result)
             for page in pages:
-                p = page - 1
-                url1 = url + str(p) + "/"
-                name = "pornbimbo-Page " + str(p)
+                url1 = self.url.replace('npage', str(page))  # + "page/" + str(page) + "/"
+                name = "milfzr-Page " + str(page)
                 name = name.upper()
                 self.cat_list.append(show_(name, url1))
             if len(self.cat_list) < 0:
@@ -337,13 +356,13 @@ class pornbimbox(Screen):
         name = self['menulist'].getCurrent()[0][0]
         url = self['menulist'].getCurrent()[0][1]
         print('pages url: ', url)
-        self.session.open(pornbimbo2, name, url)
+        self.session.open(milfzr2, name, url)
 
     def exit(self):
         self.close()
 
 
-class pornbimbo2(Screen):
+class milfzr2(Screen):
     def __init__(self, session, name, url):
         self.session = session
         Screen.__init__(self, session)
@@ -405,17 +424,222 @@ class pornbimbo2(Screen):
     def cat(self):
         self.cat_list = []
         try:
+            # content = Utils.ReadUrl2(self.url, referer)
             content = Utils.getUrl(self.url)
             if six.PY3:
                 content = six.ensure_str(content)
-            regexcat = '<div class="item.*?href="(.*?)" title="(.*?)"'
-            match = re.compile(regexcat, re.DOTALL).findall(content)
-            print("match =", match)
-            for url, name in match:
-                url1 = url
-                name = name.replace('"', '')
-                name = name.upper()
+            if 'tag' in self.url.lower():
+                regexcat = '<a href="https://milfzr.com/tag/(.*?)".*?aria-label="(.*?)"'
+                match = re.compile(regexcat, re.DOTALL).findall(content)
+                for url, name in match:
+                    # /page/2/
+                    url1 = 'https://milfzr.com/tag/' + str(url) + '/page/npage/'
+                    print('tag name= ', name)
+                    print('tag url= ', url1)
+                    name = html_conv.html_unescape(name)
+                    self.cat_list.append(show_(name, url1))
+
+            elif 'category' in self.url.lower():
+                regexcat = 'item cat.*?<a href="https://milfzr.com/category/(.*?)">(.*?)<'
+                match = re.compile(regexcat, re.DOTALL).findall(content)
+                for url, name in match:
+                    url1 = 'https://milfzr.com/category/' + str(url) + '/page/npage/'
+                    print('category name= ', name)
+                    print('category url= ', url1)
+                    name = html_conv.html_unescape(name)
+                    self.cat_list.append(show_(name, url1))
+            else:
+                print('??????????--------------------------?????????')
+
+            if len(self.cat_list) < 0:
+                return
+            else:
+                self['menulist'].l.setList(self.cat_list)
+                self['menulist'].moveToIndex(0)
+                auswahl = self['menulist'].getCurrent()[0][0]
+                self['name'].setText(str(auswahl))
+        except Exception as e:
+            print(e)
+
+    def ok(self):
+        name = self['menulist'].getCurrent()[0][0]
+        url = self['menulist'].getCurrent()[0][1]
+        try:
+            # fpage = Utils.getUrl(url)
+            # regexvideo = 'source type=".*?src="(.*?)"'
+            # match = re.compile(regexvideo, re.DOTALL).findall(fpage)
+            # url = match[0]
+            self.play_that_shit(url, name)
+        except Exception as e:
+            print(e)
+
+    def play_that_shit(self, url, name):
+        self.session.open(milfzrx2, str(name), str(url))
+
+    def exit(self):
+        self.close()
+
+
+class milfzrx2(Screen):
+    def __init__(self, session, name, url):
+        self.session = session
+        Screen.__init__(self, session)
+        skin = os.path.join(skin_path, 'defaultListScreen.xml')
+        with open(skin, 'r') as f:
+            self.skin = f.read()
+        self.menulist = []
+        self['menulist'] = rvList([])
+        self['red'] = Label(_('Back'))
+        self['title'] = Label('+18')
+        self['name'] = Label('')
+        self['poster'] = Pixmap()
+        self['text'] = Label('Only for Adult by Lululla')
+        self.currentList = 'menulist'
+        self.loading_ok = False
+        self.count = 0
+        self.loading = 0
+        self.name = name
+        self.url = url
+        self['actions'] = ActionMap(['OkCancelActions',
+                                     'ColorActions'], {'ok': self.ok,
+                                                       'cancel': self.exit,
+                                                       'red': self.exit}, -1)
+        self.timer = eTimer()
+        if Utils.DreamOS():
+            self.timer_conn = self.timer.timeout.connect(self._gotPageLoad)
+        else:
+            self.timer.callback.append(self._gotPageLoad)
+        self.timer.start(500, True)
+
+    def _gotPageLoad(self):
+        self.cat_list = []
+        name = self.name
+        try:
+            pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+            # https://milfzr.com/page/2/?s=
+            # self.urlx + '/page/npage/?s=' + str(result)
+            for page in pages:
+                url1 = self.url.replace('npage', str(page))
+                name = "milfzr-Page " + str(page)
+                # name = name.upper()
                 self.cat_list.append(show_(name, url1))
+            if len(self.cat_list) < 0:
+                return
+            else:
+                self['menulist'].l.setList(self.cat_list)
+                self['menulist'].moveToIndex(0)
+                auswahl = self['menulist'].getCurrent()[0][0]
+                self['name'].setText(str(auswahl))
+        except Exception as e:
+            print(e)
+            self['name'].setText(_('Nothing ... Retry'))
+
+    def ok(self):
+        name = self['menulist'].getCurrent()[0][0]
+        url = self['menulist'].getCurrent()[0][1]
+        print('pages url: ', url)
+        self.session.open(milfzr3, name, url)
+
+    def exit(self):
+        self.close()
+
+
+class milfzr3(Screen):
+    def __init__(self, session, name, url):
+        self.session = session
+        Screen.__init__(self, session)
+        skin = os.path.join(skin_path, 'defaultListScreen.xml')
+        with open(skin, 'r') as f:
+            self.skin = f.read()
+        self.menulist = []
+        self['menulist'] = rvList([])
+        self['red'] = Label(_('Back'))
+        # self['green'] = Label(_('Export'))
+        self['title'] = Label('+18')
+        self['name'] = Label('')
+        self['text'] = Label('Only for Adult by Lululla')
+        self['poster'] = Pixmap()
+        self.name = name
+        self.url = url
+        self.currentList = 'menulist'
+        self.loading_ok = False
+        self.count = 0
+        self.loading = 0
+        self['actions'] = ActionMap(['OkCancelActions',
+                                     'ColorActions',
+                                     'DirectionActions',
+                                     'MovieSelectionActions'], {'up': self.up,
+                                                                'down': self.down,
+                                                                'left': self.left,
+                                                                'right': self.right,
+                                                                'ok': self.ok,
+                                                                'green': self.ok,
+                                                                'cancel': self.exit,
+                                                                'red': self.exit}, -1)
+        self.timer = eTimer()
+        if Utils.DreamOS():
+            self.timer_conn = self.timer.timeout.connect(self.cat)
+        else:
+            self.timer.callback.append(self.cat)
+        self.timer.start(600, True)
+
+    def up(self):
+        self[self.currentList].up()
+        auswahl = self['menulist'].getCurrent()[0][0]
+        self['name'].setText(str(auswahl))
+
+    def down(self):
+        self[self.currentList].down()
+        auswahl = self['menulist'].getCurrent()[0][0]
+        self['name'].setText(str(auswahl))
+
+    def left(self):
+        self[self.currentList].pageUp()
+        auswahl = self['menulist'].getCurrent()[0][0]
+        self['name'].setText(str(auswahl))
+
+    def right(self):
+        self[self.currentList].pageDown()
+        auswahl = self['menulist'].getCurrent()[0][0]
+        self['name'].setText(str(auswahl))
+
+    def cat(self):
+        self.cat_list = []
+        try:
+            # content = Utils.ReadUrl2(self.url, referer)
+            content = Utils.getUrl(self.url)
+            if six.PY3:
+                content = six.ensure_str(content)
+            # clip-link" data-id="141033" title="Guys pay upfront before going all in" href="https://milfzr.com/guys-pay-upfront-before-going-all-in/">
+            regexcat = 'clip-link" data-id=.*?title="(.*?)".*?href="(.*?)"'
+            match = re.compile(regexcat, re.DOTALL).findall(content)
+            for name, url in match:
+                url1 = str(url)
+                print('name= ', name)
+                print('url= ', url1)
+                name = html_conv.html_unescape(name)
+                self.cat_list.append(show_(name, url1))
+            '''
+            # if 'tag' in self.url.lower():
+                # regexcat = '<a href="https://milfzr.com/tag/(.*?)".*?aria-label="(.*?)"'
+                # match = re.compile(regexcat, re.DOTALL).findall(content)
+                # for url, name in match:
+                    # url1 = 'https://milfzr.com/tag/' + str(url)
+                    # print('tag url= ', url1)
+                    # name = html_conv.html_unescape(name)
+                    # self.cat_list.append(show_(name, url1))
+
+            # elif 'category' in self.url.lower():
+                # regexcat = 'item cat.*?<a href="https://milfzr.com/category/(.*?)">(.*?)<'
+                # match = re.compile(regexcat, re.DOTALL).findall(content)
+                # for url, name in match:
+                    # url1 = 'https://milfzr.com/category/' + str(url)
+                    # print('category url= ', url1)
+                    # name = html_conv.html_unescape(name)
+                    # self.cat_list.append(show_(name, url1))
+            # else:
+                # print('??????????--------------------------?????????')
+            '''
             if len(self.cat_list) < 0:
                 return
             else:
@@ -431,7 +655,7 @@ class pornbimbo2(Screen):
         url = self['menulist'].getCurrent()[0][1]
         try:
             fpage = Utils.getUrl(url)
-            regexvideo = "video_url: '(.*?)'"
+            regexvideo = 'source src="(.*?)"'
             match = re.compile(regexvideo, re.DOTALL).findall(fpage)
             url = match[0]
             self.play_that_shit(url, name)

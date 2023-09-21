8e9b6a2b2afac6d92a9a25ac2685de3d408d56ce
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-09-21 20:41:50 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/xxxplugin/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/xxxplugin/plugin.py
@@ -1424,11 +1424,7 @@ class Playstream2(Screen, InfoBarBase, TvInfoBarShowHide, InfoBarSeek, InfoBarAu
         return
 
     def getAspect(self):
-        try:
-            aspect = iAVSwitch().getAspectRatioSetting()
-        except:
-            aspect = eAVSwitch().getAspectRatioSetting()
-        return aspect
+        return AVSwitch().getAspectRatioSetting()
 
     def getAspectString(self, aspectnum):
         return {0: '4:3 Letterbox',
@@ -1449,9 +1445,9 @@ class Playstream2(Screen, InfoBarBase, TvInfoBarShowHide, InfoBarSeek, InfoBarAu
                6: '16_9_letterbox'}
         config.av.aspectratio.setValue(map[aspect])
         try:
-            iAVSwitch.setAspectRatio(aspect)
+            AVSwitch().setAspectRatio(aspect)
         except:
-            eAVSwitch.setAspectRatio(aspect)
+            pass
 
     def av(self):
         temp = int(self.getAspect())
